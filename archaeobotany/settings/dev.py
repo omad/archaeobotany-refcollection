f1a8b4fb1b3af7fb3803fe4d71770a91b4f06dc2
Damien Ayers <damien@omad.net>
2012-11-30 16:27:36 +1000

    Update import to work with the new wood data schema

--- archaeobotany/settings/dev.py
+++ archaeobotany/settings/dev.py
@@ -4,3 +4,15 @@ from common import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 #########################
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'archaeobotany',                      # Or path to database file if using sqlite3.
+        'USER': 'archaeobotany',                      # Not used with sqlite3.
+        'PASSWORD': 'archaeobotany',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
